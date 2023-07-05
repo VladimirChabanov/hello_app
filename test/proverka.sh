bfe1cde734c0eedceeed507f6f78daf2c48dd4ef
Shevkie <124715654+Shevkie@users.noreply.github.com>
2023-07-05 13:18:15 +0300

    Update proverka.sh

--- test/proverka.sh
+++ test/proverka.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-output=$(curl http://127.0.0.1:5000)
+output=$(python3 /hello_app/app.py)
 
-expected_output="05.07.2023"
+today=$(date +'%Y-%m-%d')
+
+expected_output="$today"
 
 if [ "$output" == "$expected_output" ]; then
-echo "Вывод программы совпадает с ожидаемым: $output"
+    echo "Тест выполнен успешно"
 else
-echo "Вывод программы не совпадает с ожидаемым."
-exit 1
+    echo "Тест провалился"
+    echo "Ожидалось: $expected_output"
+    echo "Получено: $output"
 fi
