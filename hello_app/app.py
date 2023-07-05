72153b35faa799f04c85158d1d1926573192e89d
Alex Pi <your_email@whatever.com>
2023-07-05 11:11:08 +0300

    Changed code so program will print date and time

--- hello_app/app.py
+++ hello_app/app.py
@@ -1,11 +1,12 @@
 import os
+import datetime
 from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return f'My server IP address is {os.environ["HOST_IP"]}'
+    return f'Current date and time is  {datetime.datetime.now()}'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
