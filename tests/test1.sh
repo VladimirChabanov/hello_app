d1435bc24370263ac683f8e188e90f4bc247f726
Senchi-bit <60796068+Senchi-bit@users.noreply.github.com>
2023-07-05 12:18:09 +0300

    Update test1.sh

--- tests/test1.sh
+++ tests/test1.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-g++ app.cpp -o app
-
 output=$(curl http://127.0.0.1:5000)
 
 expected_output="05.07.2023"
@@ -10,3 +8,5 @@ if [ "$output" == "$expected_output" ]; then
 echo "Test succesfull: $output"
 else
 echo "Test failed. Expected output: $expected_output"
+exit
+fi
