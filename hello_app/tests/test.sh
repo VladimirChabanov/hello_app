bf5a7b06c50e9b96945a562e0a6bcba1e02dcf4c
JustKelmory <71424662+JustKelmory@users.noreply.github.com>
2023-07-05 18:29:11 +0300

    Update test.sh

--- hello_app/tests/test.sh
+++ hello_app/tests/test.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-output=$(python3 /hello_app/app.py)
+output=$(curl http://127.0.0.1:5000/)
 
-today=$(date +'%Y-%m-%d')
-
-expected_output="$today"
+expected_output="05.07.2023"
 
 if [ "$output" == "$expected_output" ]; then
     echo "Тест выполнен успешно"
@@ -12,4 +10,5 @@ else
     echo "Тест провалился"
     echo "Ожидалось: $expected_output"
     echo "Получено: $output"
+    exit 1
 fi
