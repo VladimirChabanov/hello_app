5962180393479ce2000e5844dd3f27e31216e18b
Alex Pi <your_email@whatever.com>
2023-07-05 11:14:16 +0300

    Changed code to print only date

--- hello_app/app.py
+++ hello_app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return f'Current date and time is  {datetime.datetime.now()}'
+    return f'Current date is  {datetime.datetime.now().date()}'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
