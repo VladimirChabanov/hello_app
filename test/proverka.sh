f90ba0d9b1d76ec1980f57680883d0fa34ce261c
Shevkie <124715654+Shevkie@users.noreply.github.com>
2023-07-05 12:17:59 +0300

    Update proverka.sh

--- test/proverka.sh
+++ test/proverka.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-g++ app.cpp -o app
-
-output=$(./app)
+output=$(curl http://127.0.0.1:5000)
 
 expected_output="05.07.2023"
 
 if [ "$output" == "$expected_output" ]; then
-echo "Тест прошел успешно: $output"
+echo "Вывод программы совпадает с ожидаемым: $output"
 else
-echo "Тест провален. Ожидаемый результат: $expected_output"
+echo "Вывод программы не совпадает с ожидаемым."
+exit 1
+fi
