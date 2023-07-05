a5bb1361ad66a1fb65e9a1d4604ac194ee360812
Alex Pi <your_email@whatever.com>
2023-07-05 11:46:37 +0300

    Added test

---
+++ tests/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+python app.py
+
+output=$(./app)
+
+expected_output="05.07.2023"
+
+if [ "$output" == "$expected_output" ]; then
+echo "Вывод программы совпадает с ожидаемым: $output"
+else
+echo "Вывод программы не совпадает с ожидаемым. Ожидается: $expected_output, >
+fi
