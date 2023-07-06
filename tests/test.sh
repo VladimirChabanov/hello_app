9d8874a7031b56fee9b1f801f91d13a4e2dca727
North-ind <129858510+North-ind@users.noreply.github.com>
2023-07-06 15:21:39 +0300

    Update test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -1,11 +1,10 @@
 #!bin/bash
-sudo apt-get -y install python3
 pip install -r requirements.txt
 python3 hello_app/app.py &
 sleep 5
 ANSW=$(curl http://127.0.0.1:5000/)
-if [ "$ANSW" != "2023-07-06" ]; then
+if [ "$ANSW" == "2023-07-06" ]; then
 echo "Successfully"
 else
-echo "$ANSW"
+echo "Error"
 fi
