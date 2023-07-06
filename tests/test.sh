04ca9e1c45baf7d28aa2e6f9eeb1361b5d026850
North-ind <129858510+North-ind@users.noreply.github.com>
2023-07-06 14:55:10 +0300

    Create test.sh

---
+++ tests/test.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+pip install -r requirements.txt
+python3 hello_app/app.py &
+ANSW=$(curl http://127.0.0.1:5000/)
+if [ "$ANSW" != "2023-07-06" ]; then
+  echo "Successfully"
+else
+  echo "Error"
+fi
