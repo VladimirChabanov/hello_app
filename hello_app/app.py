64d1a37f13d16f3806e257238bab2d7c226ba702
North-ind <129858510+North-ind@users.noreply.github.com>
2023-07-06 15:18:05 +0300

    Update app.py

--- hello_app/app.py
+++ hello_app/app.py
@@ -1,11 +1,12 @@
 import os
 from flask import Flask, request
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return f'2023-07-06'
+    return f'{datetime.now().date()}'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
