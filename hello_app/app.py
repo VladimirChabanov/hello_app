223a05538e5975280956f459e9fee6cf5b734b00
North-ind <129858510+North-ind@users.noreply.github.com>
2023-07-06 15:07:34 +0300

    Update app.py

--- hello_app/app.py
+++ hello_app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return datetime.now().date()
+    return f'{datetime.now().date()}'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
