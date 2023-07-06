f1762951eb16778300474d069577724e31be22dc
Stasoon <74395677+Stasoon@users.noreply.github.com>
2023-07-06 15:25:58 +0300

    Update main-test.sh

--- .github/workflows/main-test.sh
+++ .github/workflows/main-test.sh
@@ -12,5 +12,14 @@ jobs:
     steps:
       - name: Checkout repository
         uses: actions/checkout@v3
+        
+      - name: Set up Python
+        uses: actions/setup-python@v3
+        with:
+          python-version: '3.8'
+          
+      - name: Install dependencies
+        run: pip install -r requirements.txt
+        
       - name: Run test
         run: source tests/test.sh
