640a5407a6d234df6d276f336abd67638dbe02c0
TimurAbdurmanov2808 <57769455+TimurAbdurmanov2808@users.noreply.github.com>
2023-07-06 14:51:35 +0300

    Update app.py

--- hello_app/app.py
+++ hello_app/app.py
@@ -1,11 +1,7 @@
-import os
-from flask import Flask, request
-
-app = Flask(__name__)
-
-@app.route('/')
+app.route('/')
 def index():
     return f'My server IP address is {os.environ["HOST_IP"]}'
+    return f'06.07.2023'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
