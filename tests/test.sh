d921850e1bf7fa2b97a240335f78c1cfd493a003
VA1650 <57103544+VA1650@users.noreply.github.com>
2023-07-06 15:59:13 +0300

    Create test.sh

---
+++ tests/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+EXPECTED_DATE=$(date +"%a %b %d")
+
+ACTUAL_DATE=$(../hello_app/app.py)
+
+if [ "$EXPECTED_DATE" == "$ACTUAL_DATE" ]; then
+    echo "Test Pass"
+else
+    echo "Test Fail"
+fi
